84e1106a49667a30afda6c9aa3b513853bca2a9a
injilee99 <injilee99@gmail.com>
2024-08-10 17:41:46 +0900

    data_type, condition, loop 학습

---
+++ 2. data_type.js
@@ -0,0 +1,19 @@
+let a = 5;
+let b = 'hello'; //변수 : 변하는 수 , let만 사용
+let c = "hello";
+let d = new Date();
+let f = true;
+// var d = 1111;   // 얘는 잘 안씀. 얘는 좀 구식(옛날 구버전으로 돈다)
+
+// const d = 5;  // 변하지 않는 상수를 입력할때(ex 3.14)
+
+
+
+a = a.toString();
+d = d.toString();
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof c);
+console.log(d, typeof d);
+console.log(f, typeof f);
---
+++ 3. condition.js
@@ -0,0 +1,16 @@
+let a = 'hello world';
+
+if ( a === 'hello world' ) { // '니 마음대로 바꾸지마라' ===(3번)
+    console.log('same');
+} else {
+    console.log('diff');
+}
+
+let b = 4;
+switch (b) {
+    case 5:
+        console.log('555555');
+        break;
+    default :
+    console.log('default');
+}
